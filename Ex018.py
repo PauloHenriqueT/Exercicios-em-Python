d31cb0b513d8e8f81bf3877de3f6971ebe17b3ef
Paulo Henrique Toquetão <phtoquetao@gmail.com>
2026-02-27 16:54:45 -0300

    .

---
+++ Ex018.py
@@ -0,0 +1,8 @@
+import math
+ang = float(input('Qual o ângulo você deseja: '))
+sen = math.sin(math.radians(ang))
+print('O valor do seno é {:.2f}'.format(ang,sen))
+cos = math.cos(math.radians(ang))
+print('O valor do cosseno é: {:.2f}'.format(ang,cos))
+tan = math.tan(math.radians(ang))
+print('O valor da tangente é: {:.2f}'.format(ang,tan))
---
+++ Ex021.py
@@ -0,0 +1,4 @@
+import pygame
+pygame.init()
+pygame.mixer.music.load('Ex021.mp3')
+pygame.mixer.music.play()
